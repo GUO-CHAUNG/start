0f6a9358e190e0db4703639b1cf66efef320e444
GUO-CHAUNG <109572260+GUO-CHAUNG@users.noreply.github.com>
2022-07-20 11:20:01 +0800

    Delete src directory

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-axios.defaults.baseURL='http://e.ludir.cn/api/hkzf/public/'
-const api={
-    getlb(){
-        return axios.get('home/swiper')
-    }
-}
-export default api
